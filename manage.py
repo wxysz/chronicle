fd0dbd5e70ceba9aa274f79a2acf5432e5977ce0
wxysz <wxysz81@yahoo.com>
2021-10-12 15:35:17 +0900

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,1 +1,7 @@
+from django.http import HttpResponse
 
+def index(request):
+    # Get an HttpRequest - the request parameter
+    # perform operations using information from the request.
+    # Return HttpResponse
+    return HttpResponse('Hello from Django!')
