a86f9d9a506d8cae2792d04a97bc5eb957933e2a
wxysz <wxysz81@yahoo.com>
2021-10-12 12:33:45 +0900

    Update nv_visitor.py

--- nv_visitor.py
+++ nv_visitor.py
@@ -3,29 +3,39 @@ import os,sys
 import requests
 import xml.etree.ElementTree as ET
 import datetime
-
+import telegram
+from pytz import timezone
+ 
+KST = datetime.datetime.now(timezone('Asia/Seoul'))
+ 
 def getNVisitor():
-	naver_id = 'proevil' # 네이버 아이디
+	naver_id = 'ahnsk3939'
 	headers = {'User-Agent': 'Mozilla/5.0'}	
 	res = requests.get("https://blog.naver.com/NVisitorgp4Ajax.nhn?blogId="+naver_id,headers=headers,timeout=5)	
-	print("#########################################")
-	print(res.text)
-	print("#########################################")
+	# print("#########################################")
+	# print(res.text)
+	# print("#########################################")
 	return ET.fromstring(res.text)	
-	
 def getToDay():
-	return datetime.datetime.today().strftime("%Y%m%d")
-
+	return KST.strftime("%Y%m%d")
 def getNowTime():
-	return datetime.datetime.today().strftime("%Y.%m.%d %H시%M분")
+	return KST.strftime("%Y년%m월%d일 %H시%M분")
+
+def sendMsg(telegram_token, msgText):
+	bot 	= telegram.Bot(token = telegram_token)
+	cat_id 	= '-1001400475289'			
+	bot.sendMessage(chat_id = cat_id, text=msgText)	
 
 
 if __name__ == '__main__':
+	telegram_token = sys.argv[1]
 	try:
 		visitor_xtree = getNVisitor()		
 		for node in visitor_xtree.findall('visitorcnt'):
 			if getToDay() == node.get('id'):
-				print("%s %s명이 방문했어요!" %(getNowTime(), node.get('cnt')))
-				break											
+				msgText = ("%s %s명이 방문했어요!" %(getNowTime(), node.get('cnt')))
+				print(msgText)
+				sendMsg(telegram_token, msgText)
+				break															
 	except Exception as e:
-		print("e: ",e)
+		print("e:",e)
