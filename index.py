f916d0bf0e58a97ba41d0f19022fcdef5465d5a9
wxysz <wxysz81@yahoo.com>
2021-10-13 11:34:02 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,7 @@
+import os
+from datetime import datetime
+from pytz import timezone
+from crawling_yes24 import parsing_beautifulsoup, extract_book_data
+from github_utils import get_github_repo, upload_github_issue
+
 print("Hello, World!")
