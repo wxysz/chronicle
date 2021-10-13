c86652d474ac038a2d5326735da413d8e313cca7
wxysz <wxysz81@yahoo.com>
2021-10-13 10:23:56 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,1 @@
-from django.http import HttpResponse
-
-def index(request):
-    # Get an HttpRequest - the request parameter
-    # perform operations using information from the request.
-    # Return HttpResponse
-    return HttpResponse('Hello from Django!')
+print("Hello, World!")
