101b01ceb778adaa50656171a51128395514e663
wxysz <wxysz81@yahoo.com>
2021-10-13 16:47:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+file = open("./naver.json", "w")
+
+url = "https://search.shopping.naver.com/search/all?query=%EA%B1%B4%EC%A1%B0%EA%B8%B0&cat_id=&frm=NVSHATC"
+html = requests.get(url)
+soup = BeautifulSoup(html.text, 'lxml')
+cnt = len(soup.find_all('div', class_='basicList_title__3P9Q7'))
+
+for i in range(0,cnt) :
+    naver = {}
+    metadata = soup.find_all('div', class_='basicList_title__3P9Q7')[i]
+    title = metadata.a.get('title')
+    print("<제품명> : ", title)               # title
+    
+    price = soup.find_all('span', class_='price_num__2WUXn')[i].text
+    print("<가격> : ", price)                # 가격
+    
+    url = metadata.a.get('href')
+    print("<url> : ", url)                  # url
+         
+    print("===================================================")
+    
+    naver = {'제품명' : title , '가격' : price, 'url' : url }
+    file.write(json.dumps(naver))
+
+file.close()
+
+'''
 import json,re
 from urllib.request import urlopen
 from html import unescape
@@ -22,3 +53,4 @@ with open("booklist.json", "w", encoding="utf-8") as f:
 
     # 데이터 변형 및 추가
     json.dump(data, f, ensure_ascii=False, indent=2)
+'''
