3241fd9b03852b120edc8988e648903309fe447a
wxysz <wxysz81@yahoo.com>
2021-10-21 10:16:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,11 @@ req = requests.get('https://kleague.com/api/clubRank.do')
 req.encoding= None
 html = req.content
 soup = BeautifulSoup(html, 'html.parser')
-rank = json.loads(soup1.text)
+rank = json.loads(soup.text)
 
+with open(os.path.join(BASE_DIR, 'rank.json'), 'w+', encoding="utf-8") as f:
+    json.dump(rank, f, ensure_ascii = False, indent=2)
+    
 seoul_timezone = timezone('Asia/Seoul')
 today = datetime.now(seoul_timezone)
 today_date = today.strftime("%Y년 %m월 %d일")
@@ -24,6 +27,3 @@ repository_name = "chronicle" # 내 저장소 이름 필수로 바꿔야함
 
 g = Github(access_token)
 repo = g.get_user().get_repo(repository_name)
-
-with open(os.path.join(BASE_DIR, 'rank.json'), 'w+', encoding="utf-8") as f:
-    json.dump(rank, f, ensure_ascii = False, indent=2)
