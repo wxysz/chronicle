70cde838aba0ac6de8db9cc4a471fc85d4556fe7
wxysz <wxysz81@yahoo.com>
2021-10-21 09:50:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -28,22 +28,6 @@ for title in datas:
     url = 'http:'+title.find('a')['href']
     data[name] = url
 
-seoul_timezone = timezone('Asia/Seoul')
-today = datetime.now(seoul_timezone)
-today_date = today.strftime("%Y년 %m월 %d일")
-
-access_token = os.environ['MY_GITHUB_TOKEN']
-repository_name = "chronicle" # 내 저장소 이름 필수로 바꿔야함 
-
-g = Github(access_token)
-repo = g.get_user().get_repo(repository_name)
-
-issue_title = f"YES24 IT 신간 도서 알림({today_date})"
-
-data1 = json.loads(soup.text)
-data_json = json.dumps(data1, indent=2)
-repo.create_issue(title=issue_title, body=data_json)
-
 with open(os.path.join(BASE_DIR, 'news.json'), 'w+',encoding='utf-8') as json_file:
   json.dump(data, json_file, ensure_ascii = False, indent='\t')
 
@@ -57,10 +41,24 @@ req1.encoding= None
 html1 = req1.content
 soup1 = BeautifulSoup(html1, 'html.parser')
 rank = json.loads(soup1.text)
+
+seoul_timezone = timezone('Asia/Seoul')
+today = datetime.now(seoul_timezone)
+today_date = today.strftime("%Y년 %m월 %d일")
+
+access_token = os.environ['MY_GITHUB_TOKEN']
+repository_name = "chronicle" # 내 저장소 이름 필수로 바꿔야함 
+
+g = Github(access_token)
+repo = g.get_user().get_repo(repository_name)
+
+issue_title = f"리그 순위({today_date})"
+
+repo.create_issue(title=issue_title, body=rank)
+
 with open(os.path.join(BASE_DIR, 'rank.json'), 'w+', encoding="utf-8") as f:
     json.dump(rank, f, ensure_ascii = False)
-
-
+    
 # https://www.python2.net/questions-763617.htm
 # https://devpouch.tistory.com/33
 
